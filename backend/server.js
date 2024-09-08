6ad3972663685ce979c020051733f9e0c6d5fc17
IT21080258 <it21080258@my.sliit.lk>
2024-09-08 14:16:34 +0530

    predict-DB

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const WebSocket = require('ws');
 
 //user imports
 const GSRoutes = require('./routes/GrowShedRoute');
+const yPredictRoute = require('./routes/YieldPredictRoute');
 
 dotenv.config();
 
@@ -35,38 +36,39 @@ connection.once("open",() => {
 //routes define to be used
 //Grow shed
 app.use('/api/v1/growshed',GSRoutes);
+app.use('/api/v1/predictyield',yPredictRoute)
 
 
-const wss = new WebSocket.Server({ port: 8081 });
+// const wss = new WebSocket.Server({ port: 8081 });
 // Arduino sensor data
-const arduinoPort = new SerialPort({
-    path: 'COM5', // Change this to your actual port
-    baudRate: 9600
-});
+// const arduinoPort = new SerialPort({
+//     path: 'COM5', // Change this to your actual port
+//     baudRate: 9600
+// });
 
-const parser = arduinoPort.pipe(new ReadlineParser({ delimiter: '\n' }));
+// const parser = arduinoPort.pipe(new ReadlineParser({ delimiter: '\n' }));
 
 // Handle incoming data from Arduino
-parser.on('data', (data) => {
-    try {
-        // Parse the JSON string received from Arduino
-        const sensorData = JSON.parse(data);
-        console.log('Sensor Data:', sensorData);
+// parser.on('data', (data) => {
+//     try {
+//         // Parse the JSON string received from Arduino
+//         const sensorData = JSON.parse(data);
+//         console.log('Sensor Data:', sensorData);
 
-        // Broadcast the sensor data to all connected WebSocket clients
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(sensorData));
-            }
-        });
-    } catch (error) {
-        console.error('Error parsing sensor data:', error);
-    }
-});
+//         // Broadcast the sensor data to all connected WebSocket clients
+//         wss.clients.forEach(client => {
+//             if (client.readyState === WebSocket.OPEN) {
+//                 client.send(JSON.stringify(sensorData));
+//             }
+//         });
+//     } catch (error) {
+//         console.error('Error parsing sensor data:', error);
+//     }
+// });
 
-arduinoPort.on('error', (err) => {
-    console.error('Error opening serial port:', err);
-});
+// arduinoPort.on('error', (err) => {
+//     console.error('Error opening serial port:', err);
+// });
 
 
 
